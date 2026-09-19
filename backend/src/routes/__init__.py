21cc2871af78af9b226d63b0918e2833ec8c8ca7
Fabio <fbnawa@gmail.com>
2026-09-19 09:18:21 -0300

    feat: adiciona o crud para solicitações

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -6,6 +6,7 @@ from .auth_route import router as auth_route
 from .employee_route import router as employee_route
 from .project_route import router as project_route
 from .stage_route import router as stage_route
+from .request_route import router as request_route
 
 public_router = APIRouter()
 protected_router = APIRouter(
@@ -16,3 +17,4 @@ public_router.include_router(auth_route)
 protected_router.include_router(employee_route)
 protected_router.include_router(project_route)
 protected_router.include_router(stage_route)
+protected_router.include_router(request_route)
