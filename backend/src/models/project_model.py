5ec13b3ff2c1b701708933ea9330e41fc50371bf
Fabio <fbnawa@gmail.com>
2026-09-19 08:51:47 -0300

    feat: melhora a relação n para n entre projetos e etapas

--- backend/src/models/project_model.py
+++ backend/src/models/project_model.py
@@ -1,21 +1,60 @@
-from sqlalchemy import Enum as SQLEnum
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
 
-from src.enums.enums import Status
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import table_registry
+from src.enums.enums import Status
+
+if TYPE_CHECKING:
+    from src.models.stage_model import Stage
+
+
+project_stages = Table(
+    "project_stages",
+    table_registry.metadata,
+
+    Column(
+        "project_id",
+        ForeignKey("projects.id"),
+        primary_key=True
+    ),
+
+    Column(
+        "stage_id",
+        ForeignKey("stages.id"),
+        primary_key=True
+    )
+)
 
 
 @table_registry.mapped_as_dataclass
 class Project:
-    __tablename__ = 'projects'
+
+    __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(
-        primary_key=True, init=False, autoincrement=True
+        primary_key=True,
+        init=False,
+        autoincrement=True
     )
-    name: Mapped[str] = mapped_column(nullable=False)
-    status: Mapped[Status] = mapped_column(SQLEnum(Status), nullable=False)
+
+    name: Mapped[str] = mapped_column(
+        nullable=False
+    )
+
+    status: Mapped[Status] = mapped_column(
+        SQLEnum(Status),
+        nullable=False
+    )
+
     creator_id: Mapped[int] = mapped_column(
-        ForeignKey('employees.id'), nullable=False
+        ForeignKey("employees.id"),
+        nullable=False
+    )
+
+    stages: Mapped[list["Stage"]] = relationship(
+        secondary=project_stages,
+        back_populates="projects"
     )
--- backend/src/models/stage_model.py
+++ backend/src/models/stage_model.py
@@ -1,18 +1,39 @@
-from sqlalchemy import Enum as SQLEnum
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
 
-from src.enums.enums import Status
+from sqlalchemy import ForeignKey
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import table_registry
+from src.enums.enums import Status
+from src.models.project_model import project_stages
+
+if TYPE_CHECKING:
+    from src.models.project_model import Project
 
 
 @table_registry.mapped_as_dataclass
 class Stage:
-    __tablename__ = 'stages'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    freelancer_id: Mapped[int] = mapped_column(
-        ForeignKey('employees.id'), nullable=False
+    __tablename__ = "stages"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        init=False,
+        autoincrement=True
     )
-    status: Mapped[Status] = mapped_column(SQLEnum(Status), nullable=False)
+
+    freelancer_id: Mapped[int] = mapped_column(
+        ForeignKey("employees.id"),
+        nullable=False
+    )
+
+    status: Mapped[Status] = mapped_column(
+        SQLEnum(Status),
+        nullable=False
+    )
+
+    projects: Mapped[list["Project"]] = relationship(
+        secondary=project_stages,
+        back_populates="stages"
+    )
